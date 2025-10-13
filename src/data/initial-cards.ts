049b29e1f9f0725866db5d0363ce73a466aff3ac
Enzo prados <enzo.prados@gmail.com>
2025-10-13 16:46:06 +0000

    ah donc att, eft on fait pas un truc de créations de deck ?
    bas sinon

--- src/data/initial-cards.ts
+++ src/data/initial-cards.ts
@@ -226,17 +226,20 @@ const createRandomDeck = (): Card[] => {
     };
     
     // Adjust counts to meet deck size
-    let currentSize = Object.values(categoryCounts).reduce((a, b) => a + b, 0);
+    let currentSize = Object.values(categoryCounts).reduce((a, b) => a + (b || 0), 0);
+    
+    const allCategories = Object.keys(deckRules.categories) as CardType[];
+
     while (currentSize < deckRules.deckSize) {
-        const randomCategory = Object.keys(deckRules.categories)[getRandomInt(0, Object.keys(deckRules.categories).length - 1)] as CardType;
-        if (categoryCounts[randomCategory]! < deckRules.categories[randomCategory].max) {
+        const randomCategory = allCategories[getRandomInt(0, allCategories.length - 1)];
+        if (categoryCounts[randomCategory]! < deckRules.categories[randomCategory as keyof typeof deckRules.categories].max) {
             categoryCounts[randomCategory]!++;
             currentSize++;
         }
     }
     while (currentSize > deckRules.deckSize) {
-        const randomCategory = Object.keys(deckRules.categories)[getRandomInt(0, Object.keys(deckRules.categories).length - 1)] as CardType;
-        if (categoryCounts[randomCategory]! > deckRules.categories[randomCategory].min) {
+        const randomCategory = allCategories[getRandomInt(0, allCategories.length - 1)];
+        if (categoryCounts[randomCategory]! > deckRules.categories[randomCategory as keyof typeof deckRules.categories].min) {
             categoryCounts[randomCategory]!--;
             currentSize--;
         }
@@ -255,9 +258,10 @@ const createRandomDeck = (): Card[] => {
                 const countInDeck = deck.filter(c => c.id.startsWith(randomCardTemplate.id)).length;
                 
                 let canAdd = true;
-                if (cardRarity === 'Epic' && countInDeck >= deckRules.maxEpic) canAdd = false;
-                if (cardRarity === 'Legendary' && countInDeck >= deckRules.maxLegendary) canAdd = false;
-                if (cardRarity !== 'Epic' && cardRarity !== 'Legendary' && randomCardTemplate.type !== 'Biome' && randomCardTemplate.type !== 'Land' && countInDeck >= deckRules.maxDuplicates) canAdd = false;
+                if (cardRarity === 'Epic' && deck.filter(c => c.rarity === 'Epic').length >= deckRules.maxEpic) canAdd = false;
+                if (cardRarity === 'Legendary' && deck.filter(c => c.rarity === 'Legendary').length >= deckRules.maxLegendary) canAdd = false;
+                if (countInDeck >= deckRules.maxDuplicates && randomCardTemplate.type !== 'Land' && randomCardTemplate.type !== 'Biome') canAdd = false;
+                
 
                 if(canAdd) {
                     deck.push(instantiateCard(randomCardTemplate));
@@ -279,84 +283,7 @@ const createRandomDeck = (): Card[] => {
     return shuffleDeck(deck);
 };
 
-// For now, player gets a pre-defined deck that follows the rules
-const createPlayerDeck = (): Card[] => {
-    const deck: Card[] = [];
-    const add = (id: string, count: number) => {
-        const template = getCardTemplate(id);
-        if (template) {
-            for (let i = 0; i < count; i++) deck.push(instantiateCard(template));
-        }
-    };
-    
-    // Creatures (18)
-    // Common
-    add('goblin', 2);
-    add('elf', 2);
-    add('human_soldier', 2);
-    add('wild_wolf', 1);
-    add('guard_dog', 1);
-    add('barbarian_orc', 1);
-    add('sylvan_archer', 1);
-    add('desert_lizard', 1);
-    add('ice_fish', 1);
-    add('wandering_monk', 1);
-    // Rare
-    add('knight', 1);
-    add('cleric', 1);
-    // Epic
-    add('golem', 1);
-    // Legendary
-    add('archmage_elements', 1);
-    add('vampire', 1);
-    add('elemental_fire', 1);
-
-
-    // Spells (6)
-    add('berserk_rage', 1);
-    add('stoneskin', 1);
-    add('lightning_bolt', 1);
-    add('healing_light', 1);
-    add('giant_growth', 1);
-    add('frostbolt', 1);
-
-    // Artifacts (6)
-    add('defense_totem', 1);
-    add('scepter_of_knowledge', 1);
-    add('war_hammer', 1);
-    add('orb_of_ice', 1);
-    add('amulet_of_resistance', 1);
-    add('ring_of_fire', 1);
-
-    // Enchantments (4)
-    add('strength_enchantment', 1);
-    add('kings_blessing', 1);
-    add('fire_aura', 1);
-    add('ice_shield', 1);
-
-    // Potions (4)
-    add('health_potion', 1);
-    add('mana_potion', 1);
-    add('speed_potion', 1);
-    add('endurance_potion', 1);
-
-    // Lands (4)
-    add('forest_land', 2);
-    add('mountain_land', 2);
-
-    // Biomes (3)
-    add('forest_biome', 1);
-    add('mountain_biome', 1);
-    add('swamp_biome', 1);
-
-    return shuffleDeck(deck);
-}
-
-
 // The main function to be called by the game reducer
 export const createDeck = (type: 'player' | 'opponent'): Card[] => {
-    if (type === 'opponent') {
-        return createRandomDeck();
-    }
-    return createPlayerDeck();
+    return createRandomDeck();
 };
